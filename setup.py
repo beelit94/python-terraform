a27362a145a256f557d9f7050e10b5481c25ff0d
beelit94 <beelit94@gmail.com>
2018-03-19 18:30:49 -0700

    Bump version: 0.9.1 → 0.10.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ except IOError:
 
 setup(
     name=module_name,
-    version='0.9.1',
+    version='0.10.0',
     url='https://github.com/beelit94/python-terraform',
     license='MIT',
     author='Freddy Tan',
