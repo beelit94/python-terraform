fc584c2a480816bda8c8c65f61f0803fc3c4bb68
aubustou <charlemagnecontrelesninjas@gmail.com>
2020-10-19 23:57:22 +0200

    Migrate to Python 3 only

--- python_terraform/tfstate.py
+++ python_terraform/tfstate.py
@@ -1,14 +1,11 @@
-# -*- coding: utf-8 -*-
-# above is for compatibility of python2.7.11
-
 import json
 import os
 import logging
 
-log = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 
-class Tfstate(object):
+class Tfstate:
     def __init__(self, data=None):
         self.tfstate_file = None
         self.native_data = data
@@ -17,10 +14,11 @@ class Tfstate(object):
 
     @staticmethod
     def load_file(file_path):
+        """Read the tfstate file and load its contents.
+
+        Parses then as JSON and put the result into the object.
         """
-        Read the tfstate file and load its contents, parses then as JSON and put the result into the object
-        """
-        log.debug('read data from {0}'.format(file_path))
+        logger.debug('read data from %s', file_path)
         if os.path.exists(file_path):
             with open(file_path) as f:
                 json_data = json.load(f)
@@ -29,6 +27,6 @@ class Tfstate(object):
             tf_state.tfstate_file = file_path
             return tf_state
 
-        log.debug('{0} is not exist'.format(file_path))
+        logger.debug('%s is not exist', file_path)
 
         return Tfstate()
